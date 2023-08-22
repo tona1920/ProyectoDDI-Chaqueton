910ce386dfb6743c90fcafc3c59c66579b0dbe74
Georgio Tumbado <97042086+JorgeRico08@users.noreply.github.com>
2023-08-22 07:28:18 -0600

    Update sensor-luz.ino

--- sensor-luz.ino
+++ sensor-luz.ino
@@ -1,17 +1,22 @@
+// Librerias que se implementaron para la ESP8266 y MQTT
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+// Credenciales para la conexion con la red wifi y para que se concectara al Brokret
 const char* ssid = "George";
 const char* password = "zsns6298";
 const char* mqtt_server = "192.168.235.43";
 // const char* mqtt_server = "192.168.12.29";
 const int mqtt_port = 1883;
 
+// Inicializar las instancias deacuerdo alas librerias que declaramos
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// Creamos una variable con el pin de Datos
 const int analogLightPin = A0; // Pin A0 para el sensor de luz KY-018
 
+// Inicializamos la conexion a internet de la ESP8266
 void setup() {
   Serial.begin(9600);
 
@@ -25,6 +30,7 @@ void setup() {
 }
 
 void loop() {
+  // Validamos que este conectada a internet
   if (!client.connected()) {
     reconnect();
   }
@@ -45,6 +51,7 @@ void loop() {
   delay(1000); // Espera 1 segundo antes de enviar el próximo dato
 }
 
+// Funcion que nos permite la conexion a MQTT una vez que se le haya conecatado la red a la ESP
 void reconnect() {
   while (!client.connected()) {
     Serial.println("Conectando al servidor MQTT...");
